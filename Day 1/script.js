d28256b1bf681922f94b709cbef2d28e68238937
B1AL1 <k.konrad9902@gmail.com>
2022-05-03 22:05:20 +0200

    1st part

---
+++ Day 1/script.js
@@ -0,0 +1,26 @@
+const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya',
+]
+
+const webTechs = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Redux',
+    'Node',
+    'MongoDB',
+]
+
+console.log(countries.splice(8, 1));
+console.log(webTechs.slice(2, 3));
